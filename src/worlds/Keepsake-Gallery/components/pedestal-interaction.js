a9bcfc9b0bb278f3b6327a065b94bf0a8bed4d7e
aaron-chase <140291392+aaron-chase@users.noreply.github.com>
2025-03-16 16:09:49 -0400

    Organized scripts

---
+++ src/worlds/Keepsake-Gallery/components/pedestal-interaction.js
@@ -0,0 +1,97 @@
+AFRAME.registerComponent('pedestal-interaction', {
+    init: function () {
+        const self = this;
+        this.hasDisplayedObject = false;
+        this.displayedObjectId = null;
+        this.pedestalTop = new THREE.Vector3();
+
+        if (!self.el.classList.contains('interactive')) {
+            self.el.classList.add('interactive');
+        }
+
+        // calc top center of pedestal
+        this.updatePedestalPosition = () => {
+            this.el.object3D.getWorldPosition(this.pedestalTop);
+            this.pedestalTop.y += 1.5; // adjustable based on height
+        };
+
+        this.el.addEventListener('click', () => {
+            console.log('Pedestal clicked');
+            const manager = this.el.sceneEl.components['interaction-manager'];
+            console.log(manager);
+            const heldId = manager.pickedUpObject;
+            console.log('Currently held object ID:', heldId);
+
+            const breadEntity = document.querySelector('#bread');
+            const uploadUI = document.querySelector('#upload-ui');
+
+            // log bread in console
+            console.log('Bread entity found:', !!breadEntity);
+            if (breadEntity) {
+                console.log('Bread pickup component:', !!breadEntity.components['circles-pickup-object']);
+                if (breadEntity.components['circles-pickup-object']) {
+                    console.log('Bread pickedUp state:', breadEntity.components['circles-pickup-object'].pickedUp);
+                }
+            }
+
+            this.updatePedestalPosition();
+
+            // when clicked show upload UI if nothing is displayed or held
+            if (uploadUI && !this.hasDisplayedObject && !heldId) {
+                console.log('Showing Upload UI');
+                uploadUI.style.display = 'block';
+            }
+
+            // place bread
+            if (heldId === 'bread' && !this.hasDisplayedObject) {
+                console.log('placing bread on pedestal...');
+                this.hasDisplayedObject = true;
+                this.displayedObjectId = 'bread';
+
+                // detach bread from camera to place in scene
+                breadEntity.sceneEl.object3D.attach(breadEntity.object3D);
+
+                // setting bread world position to top of pedestal
+                breadEntity.setAttribute('position', {
+                    x: this.pedestalTop.x,
+                    y: this.pedestalTop.y,
+                    z: this.pedestalTop.z
+                });
+                breadEntity.setAttribute('rotation', '0 0 0');
+                breadEntity.setAttribute('scale', '1.5 1.5 1.5');
+                breadEntity.setAttribute('visible', true);
+
+                // update pickup state - using circles-pickup-object instead of pickupable
+                breadEntity.components['circles-pickup-object'].pickedUp = false;
+                manager.pickedUpObject = null;
+
+                console.log('Bread placed at position:', breadEntity.getAttribute('position'));
+            }
+            // handle picking up bread from pedestal
+            else if (this.hasDisplayedObject && !manager.pickedUpObject && this.displayedObjectId === 'bread') {
+                console.log('retrieving bread from pedestal');
+                this.hasDisplayedObject = false;
+                this.displayedObjectId = null;
+
+                const camera = document.querySelector('#camera');
+
+                // attach bread to camera for pickup
+                camera.object3D.attach(breadEntity.object3D);
+
+                // update pos relative to camera
+                breadEntity.setAttribute('position', {
+                    x: 0,
+                    y: -0.5, // slightly below
+                    z: -1    // infront
+                });
+
+                // update pickup state - using circles-pickup-object instead of pickupable
+                breadEntity.components['circles-pickup-object'].pickedUp = true;
+                manager.pickedUpObject = 'bread';
+            }
+        });
+
+        // initial position update
+        this.updatePedestalPosition();
+    }
+});
---
+++ src/worlds/Keepsake/components/pickupable.js
@@ -0,0 +1,90 @@
+// custom pick up
+AFRAME.registerComponent("pickupable", {
+    init: function () {
+        var self = this;
+        self.player = document.querySelector("#camera");
+        self.pickedUp = false;
+        self.clickDebounce = false; // click debounce
+
+        self.playerHolder = CIRCLES.getAvatarHolderElementBody();
+        self.origParent = self.el.parentNode;
+
+        if (!self.el.classList.contains("interactive")) {
+            self.el.classList.add("interactive");
+        }
+
+        // event handler to stop default Circles control
+        self.suppressControlsFunc = function (e) {
+            // stop circles object-pickup UI from showing
+            e.stopPropagation();
+            // preventing default behaviours
+            e.preventDefault();
+            return false;
+        };
+
+        // debounce
+        self.handleClick = function (e) {
+            if (self.clickDebounce) return;
+
+            self.clickDebounce = true;
+            setTimeout(function () {
+                self.clickDebounce = false;
+            }, 300); // 300ms debounce
+
+            console.log(
+                "clicked on object, current state:",
+                self.pickedUp ? "picked up" : "not picked up"
+            );
+
+            if (self.pickedUp === true) {
+                // release obj
+                self.origParent.object3D.attach(self.el.object3D);
+                console.log("released");
+                self.pickedUp = false;
+
+                self.el.emit("object-released", { id: self.el.id }, true);
+                self.el.sceneEl.emit(
+                    "object-released",
+                    { id: self.el.id },
+                    false
+                );
+            } else {
+                // pick up obj
+                self.playerHolder.object3D.attach(self.el.object3D);
+
+                // position in front of camera
+                self.el.setAttribute("position", {
+                    x: 0.8,
+                    y: -0.6, // slightly below center of view
+                    z: 1, // closer to camera so it appears in hand
+                });
+
+                self.el.setAttribute("rotation", {
+                    x: 0,
+                    y: 0,
+                    z: 0,
+                });
+
+                console.log("picked up");
+                self.pickedUp = true;
+                self.el.emit("object-picked-up", { id: self.el.id }, true);
+                self.el.sceneEl.emit(
+                    "object-picked-up",
+                    { id: self.el.id },
+                    false
+                );
+            }
+        };
+
+        self.el.addEventListener("click", self.handleClick);
+
+        // is obj picked up
+        self.isPickedUp = function () {
+            return self.pickedUp;
+        };
+    },
+
+    remove: function () {
+        this.el.removeEventListener("click", this.handleClick);
+    },
+});
